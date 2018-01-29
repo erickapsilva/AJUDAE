695e7f96958e6f892d6d0abae3d436e00cf14268
Ericka Pricila <ericka.psilva@ufrpe.br>
2018-01-29 19:47:59 -0300

    Fab Button e Card
    Adicionando novo fab-button e novo layout para os cards do feed

--- app/src/main/java/com/app/armetech/ajudae/user/gui/fragments/FeedFragment.java
+++ app/src/main/java/com/app/armetech/ajudae/user/gui/fragments/FeedFragment.java
@@ -10,16 +10,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.armetech.ajudae.R;
-
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 
 public class FeedFragment extends Fragment {
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton2, floatingActionButton3;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView=inflater.inflate(R.layout.fragment_feed,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_feed,container,false);
+
+        materialDesignFAM = (FloatingActionMenu) rootView.findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton2 =  (FloatingActionButton) rootView.findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = (FloatingActionButton) rootView.findViewById(R.id.material_design_floating_action_menu_item3);
+
+
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu second item clicked
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu third item clicked
+
+            }
+        });
 
         return rootView;
+
     }
 
 }
